cf92b3c87307ebddc3c9a89757b039a4677f25cf
Jeong Jinwoo <jwjeong96@gmail.com>
2022-01-30 16:37:20 +0900

    feat(fav): show fav list on new tab page

--- src/page/new/renderer.js
+++ src/page/new/renderer.js
@@ -10,9 +10,29 @@ const {
 
 const init = () => getAllFavorites();
 
+const favUlElement = document.querySelector("ul");
+
 listen_on.readAllFavorites((_, { data }) => {
   const favorites = JSON.parse(data);
-  console.log(favorites);
+  favorites.forEach((favData) => appendNewFavElement(favData));
 });
+
+const appendNewFavElement = (favData) => {
+  const { title, url, favicon } = favData;
+
+  const listContainer = document.createElement("li");
+  const _anchor = document.createElement("a");
+  const __favicon = document.createElement("img");
+  const __title = document.createElement("span");
+
+  _anchor.href = url;
+  __favicon.src = favicon;
+  __title.innerHTML = title;
+
+  _anchor.appendChild(__favicon);
+  _anchor.appendChild(__title);
+  listContainer.appendChild(_anchor);
+  favUlElement.appendChild(listContainer);
+};
 
 init();
